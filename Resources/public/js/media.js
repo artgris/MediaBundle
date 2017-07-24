1172a58d582f5300c66d8637e9c3582b704b7775
Antonin <arkanen@gmail.com>
2017-07-24 10:59:47 +0200

    Update media.js

--- Resources/public/js/media.js
+++ Resources/public/js/media.js
@@ -18,7 +18,7 @@ $(function () {
             $imagePath.change();
             $artgrisMedia.find('.img-preview').html('');
         })
-        .on('shown.bs.modal', '.modal', function() {
+        .on('shown.bs.modal', '.modal.artgris-media-modal', function() {
             var $iframe = $(this).find('.iframe');
             $iframe.on('load', function () {
                 applyIFrameEvents($(this));
